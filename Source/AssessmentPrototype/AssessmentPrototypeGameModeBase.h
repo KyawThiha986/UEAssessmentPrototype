2cc16ea7f8f9986abed920d32f5cacc47cc30786
Kyaw Thiha <alexplayer153@gmail.com>
2023-09-17 06:37:57 +1000

    Test1

---
+++ Source/AssessmentPrototype/AssessmentPrototypeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AssessmentPrototypeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSESSMENTPROTOTYPE_API AAssessmentPrototypeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
